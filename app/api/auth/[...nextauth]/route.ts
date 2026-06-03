26062a3958aaf0f2bca4944a2a88a48d51d0f946
Zius Kumar <luckykv17@gmail.com>
2026-06-03 14:55:55 +0530

    FixVercelEnvironmentVariables

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,10 @@
+if (!process.env.NEXTAUTH_SECRET) {
+  process.env.NEXTAUTH_SECRET = "mysecret123456789";
+}
+if (!process.env.NEXTAUTH_URL && process.env.VERCEL_URL) {
+  process.env.NEXTAUTH_URL = `https://${process.env.VERCEL_URL}`;
+}
+
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,10 @@
+if (!process.env.NEXTAUTH_SECRET) {
+  process.env.NEXTAUTH_SECRET = "mysecret123456789";
+}
+if (!process.env.NEXTAUTH_URL && process.env.VERCEL_URL) {
+  process.env.NEXTAUTH_URL = `https://${process.env.VERCEL_URL}`;
+}
+
 import { getServerSession } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
